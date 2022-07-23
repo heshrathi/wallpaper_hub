b4ba92818a5038a9c98479ec55689bfa7aa5c7f3
Mahesh Rathi <rmaheshrathi115@gmail.com>
2022-07-23 23:59:11 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper_hub/pages/home.dart';
+// import 'pages/home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Wallpaper Hub',
+      home: Home(),
+    ),
+  );
+}
